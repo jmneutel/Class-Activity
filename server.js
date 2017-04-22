dd1175b03a3a184019a8717a3f8cc38270e75210
Jared Neutel <Jared@Macintosh-2.local>
2017-04-22 11:11:46 -0700

    server

---
+++ server.js
@@ -0,0 +1,92 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+var tables = [];
+var waitlist = [];
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/view", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/api/:reservations?", function(req, res) {
+    var chosen = req.params.reservations;
+
+    if (chosen) {
+        console.log(chosen);
+
+        for (var i = 0; i < tables.length; i++) {
+            if (chosen === tables[i].routeName) {
+                res.json(tables[i]);
+                return;
+            }
+        }
+
+        for (var i = 0; i < waitlist.length; i++) {
+            if (chosen === waitlist[i].routeName) {
+                res.json(waitlist[i]);
+                return;
+            }
+        }
+
+
+        res.json(false);
+    } else {
+        res.json(tables);
+        res.json(waitlist);
+    }
+});
+
+app.post("/api/new", function(req, res) {
+  var newRes = req.body;
+  newRes.routeName = newRes.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newRes);
+
+  if(tables.length <= 5) {
+
+  	tables.push(newRes);
+
+  } else {
+
+  	if(waitlist.length <= 5) {
+
+  		waitlist.push(newRes);
+
+  	} else {
+
+  		console.log("Sorry, sucks to suck");
+  	}
+
+  }
+  
+  res.json(newRes);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
